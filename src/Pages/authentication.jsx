af3fac137fd864d91d67b134d0b024c028f2bfa7
zahra <zahramasoumi61@gmail.com>
2020-08-12 18:42:02 +0430

    add some features on login page

---
+++ src/Pages/authentication.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class Authentication extends React.Component {
+  render() {
+    return (
+      <div className='log-body'>
+        <div className='log-container'>
+          <div className='log-pic'>
+          <img src={"/Images/tree.png"} className="pc" alt="protection" width='80%' height='73%'/>
+          </div>
+          <div className='login-signup'>
+            bye
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/* Import statements */
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+/* App is the entry point to the React code.*/
+import App from "./App";
+// import serviceWorker from "./serviceWorker";
+/* import BrowserRouter from 'react-router-dom' */
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Suspense fallback={<div>Loading ~~~</div>}>
+      <App />
+    </Suspense>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker();
