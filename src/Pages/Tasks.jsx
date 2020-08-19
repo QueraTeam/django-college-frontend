2b93178792d9cdda25d913faf7e6d292185f24e3
zahra <zahramasoumi61@gmail.com>
2020-08-19 14:38:59 +0430

    logout request and searchbar ui done

--- src/Pages/Tasks.jsx
+++ src/Pages/Tasks.jsx
@@ -1,31 +1,48 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import { Input, Button } from 'reactstrap'
+import { IoMdPerson, IoMdLock } from 'react-icons/io'
+
 
 export default class Tasks extends React.Component {
   state = {
     token: '',
     info: [],
     trialType: {
-      imaging:'Imaging',
-      signal : 'Signal',
-      trials:'Trials'
+      imaging: 'Imaging',
+      signal: 'Signal',
+      trials: 'Trials'
 
     }
-   
+
   }
 
 
   componentDidMount() {
     var getToken = this.props.location.state.token
-    console.log('nameselected', getToken)
-    this.setState({ token: getToken  })
+    console.log('tokenlog', getToken)
+    this.setState({ token: getToken })
   }
 
   render() {
 
     return (
-      <div  >
+      <div className='taskPage' dir='rtl'>
         <Navbar token={this.state.token} />
+        <div className='searchBar'>
+          <Input placeholder='موضوع'/>
+          <Input />
+          <Input />
+          <select> 
+            <option>زن</option>
+          <option>مرد</option>
+          </select>
+          <Input />         
+          <Button > جستجو </Button>
+        </div>
+        <div className='taskContainer' >
+
+        </div>
       </div>
     )
   }
