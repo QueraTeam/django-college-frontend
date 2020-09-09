cf4df6813305f996f90ce86e2a70fddbf3e874d6
Mohammad Jafari <m.jafari9877@gmail.com>
2020-09-09 11:48:37 +0430

    fix writing

--- src/Pages/authentication.jsx
+++ src/Pages/authentication.jsx
@@ -32,7 +32,7 @@ export default class Authentication extends React.Component {
               <Tab eventKey={1} title='ورود'>
                 <Login />
               </Tab>
-              <Tab eventKey={2} title='ثبت نام'>
+              <Tab eventKey={2} title='ثبت‌نام'>
                 <SignUp />
               </Tab>
             </Tabs>
